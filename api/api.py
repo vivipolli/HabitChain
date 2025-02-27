b7c4943f64fe2ecddf9d7c6a0428acb6b9e75830
vivi <pollivivian@gmail.com>
2025-02-27 11:15:15 -0300

    update cors config

--- api/api.py
+++ api/api.py
@@ -34,6 +34,7 @@ try:
         "http://localhost:5173",
         "http://localhost:3000",
         "http://127.0.0.1:5173",
+        "https://secret-vert-six.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
