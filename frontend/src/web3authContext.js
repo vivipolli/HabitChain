e4b5a224383f69c8e4a97fc4ccf7c520d68f09e2
vivi <pollivivian@gmail.com>
2025-04-02 09:09:47 -0300

    fix page reload

--- frontend/src/web3authContext.js
+++ frontend/src/web3authContext.js
@@ -8,8 +8,7 @@ const clientId =
 const chainConfig = {
   chainNamespace: CHAIN_NAMESPACES.EIP155,
   chainId: "0x1",
-  rpcTarget:
-    import.meta.env.VITE_RPC_TARGET || "https://ethereum.publicnode.com",
+  rpcTarget: import.meta.env.VITE_RPC_TARGET,
 };
 
 const privateKeyProvider = new EthereumPrivateKeyProvider({
